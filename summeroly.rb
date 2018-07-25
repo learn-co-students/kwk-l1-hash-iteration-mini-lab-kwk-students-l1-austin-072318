2d38e9dbdc32ba11e1c8c393badcbb9da4d81431
epieklik <emilytpieklik@gmail.com>
2018-07-25 16:55:51 +0000

    Done.

--- summeroly.rb
+++ summeroly.rb
@@ -5,3 +5,8 @@ summer_olympics.merge!(:Atlanta => "1996")
  summer_olympics.each do |place, year|
  puts "The #{year} summer olympics took place in #{place}."
 end 
+
+upcased_city =[]
+summer_olympics.each_key do |place|
+  upcased_city << place.upcase
+end
