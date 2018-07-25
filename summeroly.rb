a2c9984732197040b4ddb3fb6091160c42939440
epieklik <emilytpieklik@gmail.com>
2018-07-25 16:36:35 +0000

    Done.

---
+++ summeroly.rb
@@ -0,0 +1,7 @@
+summer_olympics = {:Sydney => "2000", :Athens => "2004",
+:Beijing => "2008", :London => "2012"}
+
+summer_olympics.merge!(:Atlanta => "1996")
+ summer_olympics.each do |place, year|
+ puts "The #{year} summer olympics took place in #{place}."
+end 
